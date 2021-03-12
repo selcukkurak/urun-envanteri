f9c53b99f9b2db264cdf1e26c595e4b40708537f
Selçuk Kurak <slckkrk1996@gmail.com>
2021-03-12 12:18:30 +0300

    idari kayıtlar sayısı ve tablo bilgileri repository ve controller katmanı oluşturuldu.

--- src/main/java/tr/gov/tuik/urunenvanteri/repository/IdariKayitRepository.java
+++ src/main/java/tr/gov/tuik/urunenvanteri/repository/IdariKayitRepository.java
@@ -2,6 +2,7 @@ package tr.gov.tuik.urunenvanteri.repository;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import tr.gov.tuik.urunenvanteri.entity.IdariKayit;
 
 import java.util.List;
@@ -10,4 +11,10 @@ public interface IdariKayitRepository extends JpaRepository<IdariKayit, String> 
 
     @EntityGraph(attributePaths = {"cografiDuzey", "kullanimAmaci", "veriDuzeyi", "bicim", "aktarimBicimi", "transferPeriyot", "verininTutulduguYer", "talepBicimi", "birimDuzeyi", "iletisimKisileri"})
     List<IdariKayit> findAllBy();
+
+    @EntityGraph(attributePaths = {"tabloBilgileri"})
+    List<IdariKayit> findTablolarBy();
+
+    @Query("select count(i.id) as kayit_sayisi from IdariKayit i where i.taslak=false")
+    Object kayitSayisi();
 }
--- src/main/java/tr/gov/tuik/urunenvanteri/web/IdariKayitResource.java
+++ src/main/java/tr/gov/tuik/urunenvanteri/web/IdariKayitResource.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tr.gov.tuik.urunenvanteri.dto.IdariKayitDto;
+import tr.gov.tuik.urunenvanteri.dto.IdariKayitTabloDto;
+import tr.gov.tuik.urunenvanteri.dto.TabloBilgileriDto;
 import tr.gov.tuik.urunenvanteri.dto.mapper.IdariKayitMapper;
 import tr.gov.tuik.urunenvanteri.dto.IletisimKisiDto;
+import tr.gov.tuik.urunenvanteri.dto.mapper.IdariKayitTabloMapper;
 import tr.gov.tuik.urunenvanteri.dto.mapper.IletisimKisiMapper;
+import tr.gov.tuik.urunenvanteri.dto.mapper.TabloBilgileriMapper;
 import tr.gov.tuik.urunenvanteri.entity.IdariKayit;
 import tr.gov.tuik.urunenvanteri.exception.ResourceNotFoundException;
 import tr.gov.tuik.urunenvanteri.repository.IdariKayitRepository;
@@ -21,11 +25,17 @@ public class IdariKayitResource {
     private final IdariKayitRepository idariKayitRepository;
     private final IdariKayitMapper idariKayitMapper;
     private final IletisimKisiMapper iletisimKisiMapper;
+    private final TabloBilgileriMapper tabloBilgileriMapper;
+    private final IdariKayitTabloMapper idariKayitTabloMapper;
 
-    public IdariKayitResource(IdariKayitRepository idariKayitRepository, IdariKayitMapper idariKayitMapper, IletisimKisiMapper iletisimKisiMapper) {
+
+    public IdariKayitResource(IdariKayitRepository idariKayitRepository, IdariKayitMapper idariKayitMapper,
+                              IletisimKisiMapper iletisimKisiMapper, TabloBilgileriMapper tabloBilgileriMapper, IdariKayitTabloMapper idariKayitTabloMapper) {
         this.idariKayitRepository = idariKayitRepository;
         this.idariKayitMapper = idariKayitMapper;
         this.iletisimKisiMapper = iletisimKisiMapper;
+        this.tabloBilgileriMapper = tabloBilgileriMapper;
+        this.idariKayitTabloMapper = idariKayitTabloMapper;
     }
 
     @GetMapping
@@ -33,6 +43,12 @@ public class IdariKayitResource {
         return idariKayitRepository.findAllBy()
                 .stream()
                 .map(idariKayitMapper::toDto);
+    }
+    @GetMapping("tablolar")
+    public Stream<IdariKayitTabloDto> idariKayitTablolar() {
+        return idariKayitRepository.findTablolarBy()
+                .stream()
+                .map(idariKayitTabloMapper::toDto);
     }
 
     @GetMapping("{id}/iletisim-kisileri")
@@ -43,4 +59,15 @@ public class IdariKayitResource {
                 .orElseThrow(() -> new ResourceNotFoundException("IdariKayit", "id", id))
                 .map(iletisimKisiMapper::toDto);
     }
+    @GetMapping("{id}/tablo-bilgileri")
+    public Stream<TabloBilgileriDto> idariKayitTabloBilgileri(@PathVariable String id){
+        return idariKayitRepository.findById(id)
+                .map(IdariKayit::getTabloBilgileri)
+                .map(Collection::stream)
+                .orElseThrow(() -> new ResourceNotFoundException("IdariKayit", "id", id))
+                .map(tabloBilgileriMapper::toDto);
+    }
+
+    @GetMapping("sayi")
+    public Object kayitSayisi(){ return idariKayitRepository.kayitSayisi(); }
 }
