01429bde484f7a97721869b55ac5ca01583b3b25
Selçuk Kurak <slckkrk1996@gmail.com>
2021-03-19 11:00:24 +0300

    haber bülteni sayıları da eklendi.

--- src/main/java/tr/gov/tuik/urunenvanteri/dto/Sayilar.java
+++ src/main/java/tr/gov/tuik/urunenvanteri/dto/Sayilar.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.stream.Stream;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,7 +14,9 @@ public class Sayilar {
     private Long urunSayisi;
     private Long anketSayisi;
     private Long idariKayitSayisi;
+    private Long bultenSayisi;
     private Long kurumSayisi;
     private Long kurulusSayisi;
+
 
 }
