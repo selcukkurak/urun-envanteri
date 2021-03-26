399a103f3653323dd3c68342933bb7b344c5a253
Selçuk Kurak <slckkrk1996@gmail.com>
2021-03-26 14:24:27 +0300

    hata düzeltildi.

--- src/main/java/tr/gov/tuik/urunenvanteri/dto/mapper/MetaveriAlanMapper.java
+++ src/main/java/tr/gov/tuik/urunenvanteri/dto/mapper/MetaveriAlanMapper.java
@@ -1,7 +1,9 @@
 package tr.gov.tuik.urunenvanteri.dto.mapper;
 
 import org.mapstruct.Mapper;
+import tr.gov.tuik.urunenvanteri.dto.MetaveriAlanDto;
+import tr.gov.tuik.urunenvanteri.entity.MetaveriBaslik;
 
 @Mapper(componentModel = "spring")
-public interface MetaveriAlanMapper extends GenericMapper<MetaveriAlanMapper, tr.gov.tuik.urunenvanteri.dto.MetaveriAlanDto>{
+public interface MetaveriAlanMapper extends GenericMapper<MetaveriBaslik, MetaveriAlanDto>{
 }
--- src/main/java/tr/gov/tuik/urunenvanteri/dto/mapper/MetaveriKonuMapper.java
+++ src/main/java/tr/gov/tuik/urunenvanteri/dto/mapper/MetaveriKonuMapper.java
@@ -1,8 +1,9 @@
 package tr.gov.tuik.urunenvanteri.dto.mapper;
 
 import org.mapstruct.Mapper;
+import tr.gov.tuik.urunenvanteri.dto.MetaveriKonuDto;
 import tr.gov.tuik.urunenvanteri.entity.MetaveriKonu;
 
 @Mapper(componentModel = "spring", uses = {MetaveriIcerikMapper.class})
-public interface MetaveriKonuMapper extends GenericMapper<MetaveriKonu, MetaveriKonuMapper>{
+public interface MetaveriKonuMapper extends GenericMapper<MetaveriKonu, MetaveriKonuDto>{
 }
