2a3bf0f2b7fababe32686c1a9e3146347ecdceb0
Selçuk Kurak <slckkrk1996@gmail.com>
2021-03-29 14:21:47 +0300

    haber bülteni tablosundan gelen veriler dışarıya aktarıldı.

--- src/main/java/tr/gov/tuik/urunenvanteri/web/HaberBulteniResource.java
+++ src/main/java/tr/gov/tuik/urunenvanteri/web/HaberBulteniResource.java
@@ -4,9 +4,13 @@ import org.springframework.web.bind.annotation.*;
 import tr.gov.tuik.urunenvanteri.client.TakvimClient;
 import tr.gov.tuik.urunenvanteri.client.WebIcerikClient;
 import tr.gov.tuik.urunenvanteri.dto.Bulten;
+import tr.gov.tuik.urunenvanteri.dto.HaberBulteniDto;
 import tr.gov.tuik.urunenvanteri.dto.MetaveriDto;
 import tr.gov.tuik.urunenvanteri.dto.mapper.BultenMapper;
+import tr.gov.tuik.urunenvanteri.dto.mapper.HaberBulteniMapper;
 import tr.gov.tuik.urunenvanteri.dto.mapper.MetaveriMapper;
+import tr.gov.tuik.urunenvanteri.entity.HaberBulteni;
+import tr.gov.tuik.urunenvanteri.repository.HaberBulteniRepository;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -16,10 +20,14 @@ import java.util.stream.Stream;
 public class HaberBulteniResource {
     private final WebIcerikClient webIcerikClient;
     private final TakvimClient takvimClient;
+    private final HaberBulteniRepository haberBulteniRepository;
+    private final HaberBulteniMapper haberBulteniMapper;
 
-    public HaberBulteniResource(WebIcerikClient webIcerikClient, TakvimClient takvimClient) {
+    public HaberBulteniResource(WebIcerikClient webIcerikClient, TakvimClient takvimClient, HaberBulteniRepository haberBulteniRepository, HaberBulteniMapper haberBulteniMapper) {
         this.webIcerikClient = webIcerikClient;
         this.takvimClient = takvimClient;
+        this.haberBulteniRepository = haberBulteniRepository;
+        this.haberBulteniMapper = haberBulteniMapper;
     }
 
     @GetMapping
@@ -36,10 +44,25 @@ public class HaberBulteniResource {
     }
 
     @GetMapping("/metaveri/{id}")
-    public Stream<List<MetaveriDto>> metaverileriGetir(@PathVariable List<Long> id){
-        return  webIcerikClient.getBultenMetaverileri(id)
-                    .stream()
-                    .map(MetaveriMapper::toDtoList);
+    public Stream<List<MetaveriDto>> metaverileriGetir(@PathVariable List<Long> id) {
+        return webIcerikClient.getBultenMetaverileri(id)
+                .stream()
+                .map(MetaveriMapper::toDtoList);
 
     }
+
+    @GetMapping("yeni")
+    public Stream<HaberBulteniDto> bultenleriGetir() {
+        return haberBulteniRepository.getAllBy()
+                .stream()
+                .map(haberBulteniMapper::toDto);
+    }
+
+    @PostMapping
+    public Stream<HaberBulteniDto> bultenEkle(@RequestBody HaberBulteniDto haberBulteniDto) {
+        haberBulteniRepository.save(haberBulteniMapper.toEntity(haberBulteniDto));
+        return bultenleriGetir();
+    }
+
+
 }
