e32caf0064c37ffc6a8d8f3cdba82d0210343ed6
Selçuk Kurak <slckkrk1996@gmail.com>
2021-04-19 12:50:43 +0300

    repository sorguları güncellendi.

--- src/main/java/tr/gov/tuik/urunenvanteri/repository/MetaveriKonuRepository.java
+++ src/main/java/tr/gov/tuik/urunenvanteri/repository/MetaveriKonuRepository.java
@@ -1,5 +1,6 @@
 package tr.gov.tuik.urunenvanteri.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tr.gov.tuik.urunenvanteri.entity.MetaveriKonu;
 
@@ -7,8 +8,10 @@ import java.util.List;
 
 public interface MetaveriKonuRepository extends JpaRepository<MetaveriKonu, Long> {
 
+    @EntityGraph(attributePaths = "icerikler")
     List<MetaveriKonu> getAllBy();
 
+    @EntityGraph(attributePaths = "icerikler")
     List<MetaveriKonu> getAllByUrunId(Long id);
 
 }
